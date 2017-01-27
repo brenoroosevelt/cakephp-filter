7b652b8ae9926a929be3e2ff6fcb4bf1774eba25
breno.jesus <breno.roosevelt@gmail.com>
2017-01-27 09:31:26 -0300

    update test

--- tests/test.php
+++ tests/test.php
@@ -24,6 +24,28 @@ class Test extends TestCase
         $this->theController = new Controller($request, $response);
         $this->plugin = new FilterComponent($this->theController->components());
         
+        $this->theController->loadComponent('BRFilter.Filter');
         
+        $this->theController->Filter->addFilter(
+            'filter_id' => ['field' => 'Posts.id', 'operator'=>'='],
+            'filter_title' => ['field' => 'Posts.title', 'operator' => 'LIKE', 'explode' => 'true'],
+            'filter_category_id' => ['field'=> 'Posts.category_id', 'operator' => 'IN' ]
+        );
+
+        $this->theController->request->data = [
+            'filter_id'=> 1,
+            'filter_title' => 'test',
+            'filter_category_id'=> [1,2]
+        ]
+        
+        // get conditions
+        $conditions = $this->theController->Filter->getConditions([
+            'session' => 'filter'
+        ]);
+        $url = $this->theController->Filter->getUrl();
+        
+        $this->assertNotEmpty($conditions);
+        
+        $this->assertNotEmpty($url);
     }
 }
